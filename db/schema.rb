76d1ec01dfa0fb32421e86ac6006750f75507019
Christopher Uduekwe <christopheruduekwe@gmail.com>
2024-01-11 11:28:45 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -24,15 +24,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_26_234155) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "likes", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_likes_on_post_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
-  end
-
   create_table "posts", force: :cascade do |t|
     t.integer "author_id"
     t.string "title"
